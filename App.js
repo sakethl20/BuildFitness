5200e6373b8e08bea312ef3356fdd053e0eb100c
Saketh Lakshmanan <saketh_lakshmanan@yahoo.com>
2023-02-25 12:25:40 -0500

    git test

--- App.js
+++ App.js
@@ -93,7 +93,7 @@ export default function App() {
           options={{
             title: 'Chest',
             headerStyle: {
-              backgroundColor: 'coral',
+              backgroundColor: 'lightcoral',
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
